63818264d04d289c1fb246ff854839cd57db7483
shanthi <shanthijayasekar@gmail.com>
2014-02-24 22:54:06 -0800

    Initial Commit

---
+++ temperature/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  temperature
+//
+//  Created by user on 2/24/14.
+//  Copyright (c) 2014 ShanthiSivanesan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ temperature/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  temperature
+//
+//  Created by user on 2/24/14.
+//  Copyright (c) 2014 ShanthiSivanesan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
